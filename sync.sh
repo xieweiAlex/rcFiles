8d2f8b8da615238e70d5f869f3dc1c3179cbdbf4
Alex Xie <xieweialex@gmail.com>
2017-12-28 23:51:42 -0800

    merged sync.sh and disp.sh

---
+++ sync.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function mySync {
+	echo "start to sync"
+	cp ~/.vimrc ./
+	cp ~/.xvimrc ./
+	cp ~/Nutstore/app_config/my_btt_profile.json ./
+	cp ~/Nutstore/app_config/com.googlecode.iterm2.plist ./
+	cp ~/.gitconfig ./
+	cp ~/.bashrc ./
+	cp ~/.ideavimrc ./
+	cp ~/.tmux.conf ./
+	cp -R ~/.config/karabiner ~/
+	echo "successed to sync"
+}
+
+function myDispatch {
+	echo "start to dispatch"
+	cp .vimrc ~/
+	cp .xvimrc ~/
+	cp .gitconfig ~/
+	cp .bashrc ~/
+	cp .ideavimrc ~/
+	cp .tmux.conf ~/
+	cp -R karabiner ~/.config/
+	echo "successed to dispatch"
+}
+
+action=$1
+echo "."
+echo ".."
+echo "..."
+
+
+if [[ "$#" -ne 1 ]]; then
+	echo "one and only one parameter is allowed"
+	exit 1
+fi
+
+if [[ $action == 'sync' ]]; then
+	mySync
+elif [[ $action == 'disp' || $action == 'dispatch' ]]; then
+	myDispatch
+else 
+	echo "this is invalid para: $action"		
+	echo "should be \"sync\" or \"disp\""
+fi
+
+
+
+
+
